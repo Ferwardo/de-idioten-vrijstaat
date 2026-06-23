8fd19f570d1aae1b663cce499cc14c0087ab09cf
Ferwardo <ferresax@hotmail.com>
2026-06-23 22:24:12 +0200

    migrated the archive and constitution

---
+++ docusaurus.config.ts
@@ -0,0 +1,102 @@
+import { themes as prismThemes } from 'prism-react-renderer';
+import type { Config } from '@docusaurus/types';
+import type * as Preset from '@docusaurus/preset-classic';
+
+// This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
+
+const config: Config = {
+  title: 'De Vrijstaat der Idioten',
+  tagline: 'Be rootin, be tootin, and by god be shooting but most of all be kind',
+  favicon: 'img/favicon.ico',
+
+  // Future flags, see https://docusaurus.io/docs/api/docusaurus-config#future
+  future: {
+    v4: true, // Improve compatibility with the upcoming Docusaurus v4
+  },
+
+  // Set the production url of your site here
+  url: 'https://your-docusaurus-site.example.com',
+  // Set the /<baseUrl>/ pathname under which your site is served
+  // For GitHub pages deployment, it is often '/<projectName>/'
+  baseUrl: '/de-idioten-vrijstaat',
+
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: 'idioten', // Usually your GitHub org/user name.
+  projectName: 'de-idioten-vrijstaat', // Usually your repo name.
+
+  onBrokenLinks: 'throw',
+
+  // Even if you don't use internationalization, you can use this field to set
+  // useful metadata like html lang. For example, if your site is Chinese, you
+  // may want to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en'],
+  },
+
+  presets: [
+    [
+      'classic',
+      {
+        docs: {
+          sidebarPath: './sidebars.ts',
+        },
+        blog: {
+          showReadingTime: true,
+          feedOptions: {
+            type: ['rss', 'atom'],
+            xslt: true,
+          },
+          // Useful options to enforce blogging best practices
+          onInlineTags: 'warn',
+          onInlineAuthors: 'warn',
+          onUntruncatedBlogPosts: 'warn',
+        },
+        theme: {
+          customCss: './src/css/custom.css',
+        },
+      } satisfies Preset.Options,
+    ],
+  ],
+
+  themeConfig: {
+    // Replace with your project's social card
+    image: 'img/docusaurus-social-card.jpg',
+    colorMode: {
+      respectPrefersColorScheme: true,
+    },
+    navbar: {
+      title: 'De Vrijstaat der Idioten',
+      logo: {
+        alt: "'s Lands zegel",
+        src: 'img/logo.svg',
+      },
+      items: [
+        {
+          type: 'doc',
+          position: 'left',
+          docId: 'De-Grondwet',
+          label: 'De Grondwet',
+        },
+        {
+          type: 'docSidebar',
+          sidebarId: 'archief',
+          position: 'left',
+          label: 'Archief der Idioten',
+        },
+      ],
+    },
+    footer: {
+      style: 'dark',
+      links: [],
+      copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+    },
+    prism: {
+      theme: prismThemes.github,
+      darkTheme: prismThemes.dracula,
+    },
+  } satisfies Preset.ThemeConfig,
+};
+
+export default config;
---
+++ old/docs/Archief-der-Idioten/rechtspraak/strafbladen/Cianci-Baklava-Feta.md
@@ -0,0 +1,3 @@
+| Misdrijf | Beschrijving | Straf | Datum |
+|----------|--------------|-------|-------|
+| te laat komen | art. 37 Nggr met als reden: Hij was nog aan het eten. | Geldboete van 1 Eurodeal | 04/12/2025 |
