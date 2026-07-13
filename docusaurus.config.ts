098df6c0df4244390f3023ab293185a47fc3cd0c
Ferwardo <ferresax@hotmail.com>
2026-07-13 22:02:53 +0200

    Added github action

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -25,6 +25,8 @@ const config: Config = {
   organizationName: 'idioten', // Usually your GitHub org/user name.
   projectName: 'de-idioten-vrijstaat', // Usually your repo name.
 
+  trailingSlash: false,
+
   onBrokenLinks: 'throw',
 
   // Even if you don't use internationalization, you can use this field to set
